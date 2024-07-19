6de4428107491a1043daeca28ee35115969a112d
Vansh-renewe <vansh.kapoor@renewe.in>
2024-07-19 11:47:54 +0530

    feat: Add success message after form submission

--- src/app/(app)/contact-us/page.js
+++ src/app/(app)/contact-us/page.js
@@ -10,6 +10,7 @@ function Page() {
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
     const [error, setError] = useState('')
+    const [success, setSuccess] = useState(false)
 
     const handleSubmit = async e => {
         e.preventDefault()
@@ -29,6 +30,8 @@ function Page() {
             setEmail('')
             setMessage('')
             setSelectedOption('Services')
+            setSuccess(true)
+            setTimeout(() => setSuccess(false), 5000)
         } catch (error) {
             setError(error.response?.data?.message || 'Error submitting form')
         }
@@ -39,6 +42,18 @@ function Page() {
             <div className="text-theme-dark text-4xl mt-2">
                 Drop Us a Message
             </div>
+
+            {success && (
+                <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+                    <div className="bg-white p-8 rounded-lg text-center">
+                        <p className="text-lg">
+                            Thank you for reaching out. Your message has been
+                            received, and our team will promptly review it. You
+                            can expect to hear from us within the next 24 hours.
+                        </p>
+                    </div>
+                </div>
+            )}
 
             <form onSubmit={handleSubmit} className="mt-12">
                 <div className="flex flex-col md:flex-row md:gap-10 gap-5">
--- src/components/ContactModal.js
+++ src/components/ContactModal.js
@@ -10,6 +10,7 @@ function ContactModal({ isModalOpen, setIsModalOpen }) {
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
     const [error, setError] = useState('')
+    const [success, setSuccess] = useState(false)
 
     const handleSubmit = async e => {
         e.preventDefault()
@@ -29,6 +30,8 @@ function ContactModal({ isModalOpen, setIsModalOpen }) {
             setEmail('')
             setMessage('')
             setSelectedOption('Services')
+            setSuccess(true)
+            setTimeout(() => setSuccess(false), 5000)
             setIsModalOpen(false)
         } catch (error) {
             setError(error.response?.data?.message || 'Error submitting form')
@@ -36,99 +39,115 @@ function ContactModal({ isModalOpen, setIsModalOpen }) {
     }
 
     return (
-        <div
-            className={`z-50 fixed inset-0 flex justify-center items-center ${
-                isModalOpen ? 'visible backdrop-blur-md' : 'invisible'
-            }`}>
-            <div className="modal-box relative lg:w-96 p-8 mx-2 rounded-lg shadow-lg bg-white">
-                <form onSubmit={handleSubmit}>
-                    <button
-                        type="button"
-                        className="btn btn-sm btn-circle btn-ghost absolute right-4 top-4"
-                        onClick={() => setIsModalOpen(false)}>
-                        ✕
-                    </button>
-                    <div className="flex flex-col gap-4">
-                        <label className="pt-0 label label-text font-semibold">
-                            <span>Drop Us a Message</span>
-                        </label>
-                        <select
-                            className={`border rounded-lg border-gray-300 block w-full p-3 text-lg ${
-                                selectedOption === 'Services'
-                                    ? 'text-gray-400'
-                                    : 'text-black'
-                            }`}
-                            value={selectedOption}
-                            onChange={e => setSelectedOption(e.target.value)}
-                            required>
-                            <option value="Services" disabled>
-                                Services
-                            </option>
-                            <option value="Design & Engineering">
-                                Design & Engineering
-                            </option>
-                            <option value="Project Management Consultancy">
-                                Project Management Consultancy
-                            </option>
-                            <option value="Procurement Consultancy">
-                                Procurement Consultancy
-                            </option>
-                            <option value="Owners Engineers">
-                                Owners Engineers
-                            </option>
-                            <option value="Quality Inspection">
-                                Quality Inspection
-                            </option>
-                            <option value="IMS">IMS</option>
-                            <option value="ESG">ESG</option>
-                            <option value="EPC">EPC</option>
-                            <option value="Investment for PPA">
-                                Investment for PPA
-                            </option>
-                            <option value="Other">Other</option>
-                        </select>
-                        <input
-                            type="text"
-                            className="border rounded-lg border-gray-300 block w-full p-3 text-lg"
-                            placeholder="Your Name *"
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                            required
-                        />
-                        <input
-                            type="text"
-                            className="border rounded-lg border-gray-300 block w-full p-3 text-lg"
-                            placeholder="Your Phone *"
-                            value={phone}
-                            onChange={e => setPhone(e.target.value)}
-                            required
-                        />
-                        <input
-                            type="email"
-                            className="border rounded-lg border-gray-300 block w-full p-3 text-lg"
-                            placeholder="Your Email *"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                            required
-                        />
-                        <textarea
-                            className="border rounded-lg border-gray-300 block w-full p-3 text-lg"
-                            placeholder="Your Message *"
-                            rows="4"
-                            value={message}
-                            onChange={e => setMessage(e.target.value)}
-                            required
-                        />
-                        {error && <p className="text-red-500">{error}</p>}
-                        <button
-                            type="submit"
-                            className="bg-theme-default px-4 py-2 text-lg text-white rounded-lg">
-                            Send Message
-                        </button>
+        <>
+            {success && (
+                <div className="fixed inset-0 flex justify-center items-center bg-gray-800 bg-opacity-75 z-50">
+                    <div className="bg-white p-8 rounded-lg text-center">
+                        <p className="text-lg">
+                            Thank you for reaching out. Your message has been
+                            received, and our team will promptly review it. You
+                            can expect to hear from us within the next 24 hours.
+                        </p>
                     </div>
-                </form>
+                </div>
+            )}
+
+            <div
+                className={`z-50 fixed inset-0 flex justify-center items-center ${
+                    isModalOpen ? 'visible backdrop-blur-md' : 'invisible'
+                }`}>
+                <div className="modal-box relative lg:w-96 p-8 mx-2 rounded-lg shadow-lg bg-white">
+                    <form onSubmit={handleSubmit}>
+                        <button
+                            type="button"
+                            className="btn btn-sm btn-circle btn-ghost absolute right-4 top-4"
+                            onClick={() => setIsModalOpen(false)}>
+                            ✕
+                        </button>
+                        <div className="flex flex-col gap-4">
+                            <label className="pt-0 label label-text font-semibold">
+                                <span>Drop Us a Message</span>
+                            </label>
+                            <select
+                                className={`border rounded-lg border-gray-300 block w-full p-3 text-lg ${
+                                    selectedOption === 'Services'
+                                        ? 'text-gray-400'
+                                        : 'text-black'
+                                }`}
+                                value={selectedOption}
+                                onChange={e =>
+                                    setSelectedOption(e.target.value)
+                                }
+                                required>
+                                <option value="Services" disabled>
+                                    Services
+                                </option>
+                                <option value="Design & Engineering">
+                                    Design & Engineering
+                                </option>
+                                <option value="Project Management Consultancy">
+                                    Project Management Consultancy
+                                </option>
+                                <option value="Procurement Consultancy">
+                                    Procurement Consultancy
+                                </option>
+                                <option value="Owners Engineers">
+                                    Owners Engineers
+                                </option>
+                                <option value="Quality Inspection">
+                                    Quality Inspection
+                                </option>
+                                <option value="IMS">IMS</option>
+                                <option value="ESG">ESG</option>
+                                <option value="EPC">EPC</option>
+                                <option value="Investment for PPA">
+                                    Investment for PPA
+                                </option>
+                                <option value="Other">Other</option>
+                            </select>
+                            <input
+                                type="text"
+                                className="border rounded-lg border-gray-300 block w-full p-3 text-lg"
+                                placeholder="Your Name *"
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="text"
+                                className="border rounded-lg border-gray-300 block w-full p-3 text-lg"
+                                placeholder="Your Phone *"
+                                value={phone}
+                                onChange={e => setPhone(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="email"
+                                className="border rounded-lg border-gray-300 block w-full p-3 text-lg"
+                                placeholder="Your Email *"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                                required
+                            />
+                            <textarea
+                                className="border rounded-lg border-gray-300 block w-full p-3 text-lg"
+                                placeholder="Your Message *"
+                                rows="4"
+                                value={message}
+                                onChange={e => setMessage(e.target.value)}
+                                required
+                            />
+                            {error && <p className="text-red-500">{error}</p>}
+                            <button
+                                type="submit"
+                                className="bg-theme-default px-4 py-2 text-lg text-white rounded-lg">
+                                Send Message
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
